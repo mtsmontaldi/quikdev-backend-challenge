70d975a242c082d7283f7d7266c8e34b0a5a2dbd
marceloWired <marcelo.santosdev1@gmail.com>
2021-11-18 12:09:50 -0300

    test: ensure that UserCreateController returns 400 if any required field is not provided

---
+++ src/presentation/controllers/user/user-create.spec.ts
@@ -0,0 +1,140 @@
+import { UserCreateController } from './user-create'
+
+interface SutTypes {
+  sut: UserCreateController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new UserCreateController()
+  return {
+    sut
+  }
+}
+
+describe('CreateUser Controller', () => {
+  test('Should return 400 if no name is provided', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      username: 'valid_username',
+      birthdate: 'valid_birthdate',
+      address: 'valid_address',
+      addressNumber: 'valid_address_number',
+      primaryPhone: '(11) 11111-1111',
+      description: 'valid_description'
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error())
+  })
+
+  test('Should return 400 if no username is provided', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      name: 'valid_name',
+      birthdate: 'valid_birthdate',
+      address: 'valid_address',
+      addressNumber: 'valid_address_number',
+      primaryPhone: '(11) 11111-1111',
+      description: 'valid_description'
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error())
+  })
+
+  test('Should return 400 if no birthdate is provided', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      name: 'valid_name',
+      username: 'valid_username',
+      address: 'valid_address',
+      addressNumber: 'valid_address_number',
+      primaryPhone: '(11) 11111-1111',
+      description: 'valid_description'
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error())
+  })
+
+  test('Should return 400 if no address is provided', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      name: 'valid_name',
+      username: 'valid_username',
+      birthdate: 'valid_birthdate',
+      addressNumber: 'valid_address_number',
+      primaryPhone: '(11) 11111-1111',
+      description: 'valid_description'
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error())
+  })
+
+  test('Should return 400 if no addressNumber is provided', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      name: 'valid_name',
+      username: 'valid_username',
+      birthdate: 'valid_birthdate',
+      address: 'valid_address',
+      primaryPhone: '(11) 11111-1111',
+      description: 'valid_description'
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error())
+  })
+
+  test('Should return 400 if no primaryPhone is provided', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      name: 'valid_name',
+      username: 'valid_username',
+      birthdate: 'valid_birthdate',
+      address: 'valid_address',
+      addressNumber: 'valid_address_number',
+      description: 'valid_description'
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error())
+  })
+
+  test('Should return 400 if no description is provided', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      name: 'valid_name',
+      username: 'valid_username',
+      birthdate: 'valid_birthdate',
+      address: 'valid_address',
+      addressNumber: 'valid_address_number',
+      primaryPhone: '(11) 11111-1111'
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error())
+  })
+})
