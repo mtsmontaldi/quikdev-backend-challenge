ef474cde29bbf0532a45efb401055373563c4468
marceloWired <marcelo.santosdev1@gmail.com>
2021-11-18 12:09:39 -0300

    feat: ensure that UserCreateController returns 400 if any required field is not provided

---
+++ src/presentation/controllers/user/user-create.ts
@@ -0,0 +1,16 @@
+import { Controller, HttpResponse } from '../../protocols'
+
+export class UserCreateController implements Controller {
+  async handle (httpRequest: any): Promise<HttpResponse> {
+    const requiredFields = ['name', 'username', 'birthdate', 'address', 'addressNumber', 'primaryPhone', 'description']
+
+    for (const field of requiredFields) {
+      if (!httpRequest[field]) {
+        return {
+          statusCode: 400,
+          body: new Error()
+        }
+      }
+    }
+  }
+}
