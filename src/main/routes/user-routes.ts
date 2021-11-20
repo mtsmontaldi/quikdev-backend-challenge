c0e31e67471d4578d446a1518fa3b60e5c0478b9
marceloWired <marcelo.santosdev1@gmail.com>
2021-11-20 19:51:05 -0300

    feat: add ExpressRouteAdapter

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
+import { makeUserCreateController } from '../factories/user-create'
+import { adaptRoute } from '../adapters/express-route-adapter'
 
 export default (router: Router): void => {
-  router.post('/user', (req, res) => {
-    res.json({ ok: 'ok' })
-  })
+  router.post('/user', adaptRoute(makeUserCreateController()))
 }
