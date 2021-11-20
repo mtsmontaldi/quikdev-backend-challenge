d8512f1a8cb7faccdfe4a292f421a0ebc1748606
marceloWired <marcelo.santosdev1@gmail.com>
2021-11-20 18:58:57 -0300

    feat: connect to mongodb before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://quikdev:quikdev_123@cluster0.jqreg.mongodb.net/quikdev-backend-challenge?retryWrites=true&w=majority',
+  port: process.env.PORT || 5050
+}
