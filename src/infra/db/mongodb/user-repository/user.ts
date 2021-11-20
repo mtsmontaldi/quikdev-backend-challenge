bf55934c5d5ff02b1f140ec6204c5f3aaf777215
marceloWired <marcelo.santosdev1@gmail.com>
2021-11-20 18:29:39 -0300

    refactor: userMongoRepository now implements AddUserRepository interface

--- src/infra/db/mongodb/user-repository/user.ts
+++ src/infra/db/mongodb/user-repository/user.ts
@@ -1,8 +1,9 @@
 import { AddUserModel, UserModel } from './user-protocols'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { userMapper } from './user-mapper'
+import { AddUserRepository } from '../../../../data/protocols/add-user-repository'
 
-export class UserMongoRepository {
+export class UserMongoRepository implements AddUserRepository {
   async add (userData: AddUserModel): Promise<UserModel> {
     const userCollection = MongoHelper.getCollection('users')
 
